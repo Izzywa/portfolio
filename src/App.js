0fb31eaf63f8d9ee559991869520b0658b1034e6
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2024-12-17 12:00:50 +0800

    first commit:

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>portfolio</h1>
+      <p>portfolio content here</p>
+    </div>
+  );
+}
+
+export default App;
