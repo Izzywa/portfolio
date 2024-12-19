7f3785aaf4f3047655cc0601eeaa8e08eaeb9fb7
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2024-12-19 18:54:02 +0800

    profil and project buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,34 @@ import 'swiper/css'
 
 export default function Home(props){
     const swiperRef = useRef();
+    const x = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+    "Etiam non tempus eros, at commodo dolor. Nam facilisis gravida massa ut auctor. Cras eget mauris id risus tincidunt varius." + 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " 
 
-    const list = ["Django","Web development", "React", "User Interface", "Github", "Django","Web development", "React", "User Interface", "Github"]
+    const list = [
+        {
+            title: "react",
+            text:x
+        },
+        {
+            title: "web development",
+            text: x
+        },
+        {
+            title: "django",
+            text:x
+        },
+        {
+            title: "Github",
+            text:x
+        }
+        ,{
+            title: "User Interface",
+            text:x
+        }
+    ]
 
     function Card(props) {
         let className = "skill-card";
@@ -16,7 +42,11 @@ export default function Home(props){
             className += " next-card"
         }
 
-        function slideTo(event){
+        if (props.isVisible) {
+            className += " visible"
+        }
+
+        function slideTo(){
             let slideIndex = props.index - 1
             if (slideIndex < 0 || slideIndex >= list.length) {
                 slideIndex = list.length - 1 
@@ -24,33 +54,43 @@ export default function Home(props){
             swiperRef.current.swiper.slideToLoop(slideIndex)
         }
         return(
-            <div className={className} onClick={slideTo} data-title={props.item}>
+            <div className={className} onClick={slideTo} data-title={props.item.title}>
                 <h6 className="card-title">
-                    {props.item}</h6>
+                    {props.item.title}</h6>
+                <div className="card-text">
+                    <p>{props.item.text}</p>
+                </div>
             </div>
         )
     }
 
     return(
         <div className="wrapper row">
-            <div className="container col-sm-6 col-12">
+            <div className="container col-md-6 col-12">
                 <h3>Hi, I'm</h3>
                 <h1>Izzatul Najwa</h1>
                 <h3>welcome to my portfolio</h3>
             </div>
-            <div className="container col-sm-6 col-12 scroll-wrapper">
+            <div className="container col-md-6 col-12 scroll-wrapper">
             <Swiper
                     spaceBetween={0}
                     slidesPerView={3}
                     loop
                     ref={swiperRef}
+                    watchSlidesProgress
                 >
                 {list.map((item, index) => {
                     return(
                         <SwiperSlide key={index}>
-                           {({ isActive, isNext }) => (
-                            <Card item={item} isActive={isActive} isNext={isNext} index={index}/>
-    )}
+                           {({ isActive, isNext, isVisible}) => (
+                            <Card 
+                                item={item} 
+                                isActive={isActive} 
+                                isNext={isNext} 
+                                index={index}
+                                isVisible={isVisible}
+                                />
+                            )}                      
                         </SwiperSlide>
                     )
                 })}
@@ -59,22 +99,3 @@ export default function Home(props){
         </div>
     )
 }
-
-/**
- * <Swiper
-                    spaceBetween={0}
-                    slidesPerView={3}
-                    loop
-                    ref={swiperRef}
-                >
-                {list.map((item, index) => {
-                    return(
-                        <SwiperSlide key={index}>
-                           {({ isActive, isNext }) => (
-                            <Card item={item} isActive={isActive} isNext={isNext} index={index}/>
-    )}
-                        </SwiperSlide>
-                    )
-                })}
-                </Swiper>
- */
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,6 +4,8 @@ import Home from "./Home";
 export default function Homepage(props) {
     return (
         <div>
+            <div className="border-btn">My Profile</div>
+            <div className="border-btn">My Projects</div>
             <Home/>
         </div>
     )
