a8c3344a572511437c92969ba3bbe09b8f39cc6d
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2024-12-20 16:18:58 +0800

    animation of accordion fail

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
@@ -13,10 +13,14 @@ export default function Profile(props) {
                 core values
             </p>
      */
+
+    const profileDivRef = useRef();
+    const workExpRef = useRef();
+    const eduRef = useRef();
+
     const linkedinURL = "https://www.linkedin.com/in/izzatul-najwa-binti-ismail/"
     const workEmail = " ismail.izzatulnajwa@gmail.com"
     const phoneNum = "+6019 590 7266"
-
     const profileDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
     Etiam non tempus eros, at commodo dolor. Nam facilisis gravida massa ut auctor. Cras eget mauris id risus tincidunt varius. \
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
@@ -27,23 +31,22 @@ export default function Profile(props) {
         window.location.href = linkedinURL
     }
 
-    function DivExpand() {
-        console.log('expand div')
-    }
-
     return(
         <div className="wrapper profile-wrapper">
         <div className="row">
             <div className="work-exp-div col-lg-4 col-12">
-                <div className="d-lg-none sm-view-div" onClick={DivExpand}>
+                <div className="d-lg-none sm-view-div">
                     <h6>Work experience</h6>
+                    <div className="work-exp" ref={workExpRef}>
+                        <p>{profileDesc}</p>
+                    </div>
                 </div>
                 <div className="d-none d-lg-block lg-view-div">
                     big view 
                 </div>
             </div>
 
-            <div className="col-lg-4 col-12 profile-div">
+            <div className="col-lg-4 col-12 profile-div" ref={profileDivRef}>
                 <div className="contact-div text-center">
                         <p onClick={redirectLinkedin}><LinkedInIcon/> LinkedIn</p>
                         <p><MailOutlineIcon/> {workEmail}</p>
@@ -58,7 +61,10 @@ export default function Profile(props) {
                     </div>
 
                 <div className="education-div col-lg-4 col-12">
-                <div className="d-lg-none sm-view-div" onClick={DivExpand}>
+                <div className="d-lg-none sm-view-div">
+                    <div className="edu-list" ref={eduRef}>
+                        {profileDesc}
+                    </div>
                 <h6>education</h6>
                 </div>
                 <div className="d-none d-lg-block lg-view-div">
